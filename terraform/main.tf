b770a1d963e44de3c3704decc844041d990d08ad
jason tsai <jason.tsaicc@gmail.com>
2025-06-03 23:45:06 +0800

    Add Network Security Group and update variables for VM configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,58 @@ terraform {
   }
 }
 
+# 1) 建立共用的 Network Security Group
+
+resource "oci_core_network_security_group" "shared_nsg" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = var.vcn_ocid        # 你要放在哪個 VCN 底下，請替換成實際變數
+  display_name   = var.nsg_display_name
+}
+# 範例：在 NSG 裡允許 HTTP (80) 及 SSH (22) Ingress，和所有 Egress
+# SSH ingress rule
+resource "oci_core_network_security_group_security_rule" "allow_ssh" {
+  network_security_group_id = oci_core_network_security_group.shared_nsg.id
+  direction                 = "INGRESS"
+  protocol                  = "6" # TCP
+  source                    = "36.229.26.193/32"
+  tcp_options {
+    destination_port_range {
+      min = 22
+      max = 22
+    }
+  }
+  description = "Allow SSH from anywhere"
+}
+
+# HTTP ingress rule
+resource "oci_core_network_security_group_security_rule" "allow_http" {
+  network_security_group_id = oci_core_network_security_group.shared_nsg.id
+  direction                 = "INGRESS"
+  protocol                  = "6" # TCP
+  source                    = "0.0.0.0/0"
+  tcp_options {
+    destination_port_range {
+      min = 443
+      max = 443
+    }
+  }
+  description = "Allow HTTP from anywhere"
+}
+
+# Egress: allow all outbound traffic
+resource "oci_core_network_security_group_security_rule" "allow_all_egress" {
+  network_security_group_id = oci_core_network_security_group.shared_nsg.id
+  direction                 = "EGRESS"
+  protocol                  = "all"
+  destination               = "0.0.0.0/0"
+  description               = "Allow all outbound traffic"
+}
 
 
+
+# 2) 如果需要 Data Volume，先建立 Volume 再掛到 VM
+
+# 第一臺 VM
 resource "oci_core_instance" "arm_vm_1" {
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_ocid
@@ -23,13 +73,20 @@ resource "oci_core_instance" "arm_vm_1" {
   create_vnic_details {
     subnet_id        = var.subnet_ocid
     assign_public_ip = true
-    display_name     = "free-arm-vnic-1"
-    hostname_label   = "freearmvm-1"
+    display_name     = "${var.vm1_display_name}-vnic"
+    hostname_label   = var.vm1_hostname_label
+    private_ip      = var.vm1_private_ip 
+
+    # 把 VM1 的 VNIC 指定到同一個 NSG
+    nsg_ids = [oci_core_network_security_group.shared_nsg.id]
+
   }
 
   source_details {
     source_type = "image"
     source_id   = var.image_ocid
+    boot_volume_size_in_gbs  = var.boot_volume_size_in_gbs
+
   }
 
   metadata = {
@@ -42,7 +99,7 @@ resource "oci_core_instance" "arm_vm_1" {
 resource "oci_core_instance" "arm_vm_2" {
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_ocid
-  display_name        = "free-arm-vm-2"
+  display_name        = var.vm2_display_name
   shape               = "VM.Standard.A1.Flex"
 
   shape_config {
@@ -53,13 +110,16 @@ resource "oci_core_instance" "arm_vm_2" {
   create_vnic_details {
     subnet_id        = var.subnet_ocid
     assign_public_ip = true
-    display_name     = "free-arm-vnic-2"
-    hostname_label   = "freearmvm-2"
+    display_name     = "${var.vm2_display_name}-vnic"
+    hostname_label   = var.vm2_hostname_label
+    private_ip      = var.vm2_private_ip 
+    nsg_ids = [oci_core_network_security_group.shared_nsg.id]
   }
 
   source_details {
     source_type = "image"
     source_id   = var.image_ocid
+    boot_volume_size_in_gbs  = var.boot_volume_size_in_gbs
   }
 
   metadata = {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,84 @@
-variable "compartment_ocid" {}
-variable "availability_domain" {}
-variable "subnet_ocid" {}
-variable "ssh_public_key" {}
-variable "region" {}
+# variables.tf
+variable "region" {
+  description = "Oracle Cloud Infrastructure 的 Region"
+  type        = string
+}
+variable "vcn_ocid" {
+  description = "要放在哪個 VCN 底下，請替換成實際變數"
+  type        = string
+}
+variable "compartment_ocid" {
+  description = "要建立資源的 Compartment OCID"
+  type        = string
+}
+
+variable "availability_domain" {
+  description = "要放在哪個 Availability Domain，例如 'Uocm:AP-TPE-AD-1'"
+  type        = string
+}
+
+variable "subnet_ocid" {
+  description = "VM VNIC 要放入的 Subnet OCID"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "用來放到 metadata 裡的 SSH 公鑰內容（一整行）"
+  type        = string
+}
+
 variable "image_ocid" {
-  description = "The OCID of the image to use for the instance"
+  description = "要用的 Image OCID，例如 Oracle 提供的 Ubuntu 24.04 映像"
+  type        = string
+}
+
+# Boot Volume 大小（GB），預設 50GB，可透過 CLI/terraform.tfvars 覆蓋
+variable "boot_volume_size_in_gbs" {
+  description = "Boot Volume size"
+  type        = number
+  default     = 50
+}
+
+# Data Volume（額外掛載磁碟）是否要使用，以及大小
+variable "attach_data_volume" {
+  description = "是否要建立並 attach 一個額外的 Data Volume"
+  type        = bool
+  default     = false
+}
+
+# NSG 的 display name
+variable "nsg_display_name" {
+  description = "要建立的 Network Security Group 名稱"
+  type        = string
+  default     = "shared-vm-nsg"
+}
+
+# VM 1 跟 VM 2 的顯示名稱，也可改成變數
+variable "vm1_display_name" {
+  description = "第一臺 VM 的顯示名稱"
+  type        = string
+  default     = "free-arm-vm-1"
+}
+
+variable "vm2_display_name" {
+  description = "第二臺 VM 的顯示名稱"
+  type        = string
+  default     = "free-arm-vm-2"
+}
+
+variable "vm1_hostname_label" {
+  type        = string
+  default     = "JasonArmVM1"
+}
+
+variable "vm2_hostname_label" {
+  type        = string
+  default     = "JasonArmVM2"
+}
+
+variable "vm1_private_ip" {
+  type        = string
+}
+variable "vm2_private_ip" {
   type        = string
 }
